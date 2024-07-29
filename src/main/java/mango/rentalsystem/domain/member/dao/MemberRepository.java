89dfd6355aef13dd7a04fc4d8971c11d90e146b4
ehddn6570 <ehddn6570@gmail.com>
2024-07-30 03:42:34 +0900

    feat: memberRepository 구현

--- src/main/java/mango/rentalsystem/domain/member/dao/MemberRepository.java
+++ src/main/java/mango/rentalsystem/domain/member/dao/MemberRepository.java
@@ -1,7 +1,13 @@
 package mango.rentalsystem.domain.member.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import mango.rentalsystem.domain.member.domain.Member;
+
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Member findByStudentId(String studentId);
+
 }
