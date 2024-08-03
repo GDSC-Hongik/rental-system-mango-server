166f9efabedac572b5e991a1c127eac60ffe18b0
yeonee911 <128215774+yeonee911@users.noreply.github.com>
2024-08-03 17:54:22 +0900

    카테고리 조회 기능 구현

--- src/main/java/mango/rentalsystem/domain/category/api/CategoryController.java
+++ src/main/java/mango/rentalsystem/domain/category/api/CategoryController.java
@@ -1,7 +1,28 @@
 package mango.rentalsystem.domain.category.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import mango.rentalsystem.domain.category.application.CategoryService;
+import mango.rentalsystem.domain.category.dto.response.CategoryResponse;
+
 @RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+	private final CategoryService categoryService;
+
+	// 카테고리 전체 조회
+	@GetMapping
+	public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+		List<CategoryResponse> response = categoryService.findAll();
+		return ResponseEntity.ok().body(response);
+	}
 }
--- src/main/java/mango/rentalsystem/domain/category/application/CategoryService.java
+++ src/main/java/mango/rentalsystem/domain/category/application/CategoryService.java
@@ -1,19 +1,30 @@
 package mango.rentalsystem.domain.category.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import mango.rentalsystem.domain.category.dao.CategoryRepository;
+import mango.rentalsystem.domain.category.domain.Category;
+import mango.rentalsystem.domain.category.dto.response.CategoryResponse;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryService {
 	private final CategoryRepository categoryRepository;
 
 	/**
-	 * 카테고리 조회
+	 * 카테고리 전체 조회
 	 */
-	// MEMBER인지 검사
+	public List<CategoryResponse> findAll() {
+		List<Category> categories = categoryRepository.findAll();
+		return categories.stream().map(CategoryResponse::of).collect(Collectors.toList());
+	}
+
 
 	/**
 	 * 카테고리 추가
