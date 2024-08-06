6d0fb52a7e4a93be38af238c8d9c53d2d7929b8c
Jaewon Kim <eshcirc1e@gmail.com>
2024-08-06 16:35:54 +0900

    feat: Department 도메인 Controller, Service, Repository 구현

--- src/main/java/mango/rentalsystem/domain/department/api/DepartmentController.java
+++ src/main/java/mango/rentalsystem/domain/department/api/DepartmentController.java
@@ -3,7 +3,7 @@ package mango.rentalsystem.domain.department.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mango.rentalsystem.domain.auth.domain.LoginUser;
 import mango.rentalsystem.domain.department.application.DepartmentService;
-import mango.rentalsystem.domain.department.dto.request.DepartmentUpdateRequest;
+import mango.rentalsystem.domain.department.dto.request.DepartmentRentalTimeUpdateRequest;
+import mango.rentalsystem.domain.department.dto.request.DepartmentInfoUpdateRequest;
 import mango.rentalsystem.domain.department.dto.response.DepartmentResponse;
 
 @RestController
@@ -25,13 +26,22 @@ public class DepartmentController {
 	@GetMapping
 	@PreAuthorize("hasRole('MEMBER')")
 	public ResponseEntity<DepartmentResponse> getDepartment(@LoginUser String studentId) {
+		return ResponseEntity.ok(departmentService.getDepartment(studentId));
+	}
+
+	@PutMapping
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Void> updateDepartmentInfo(@LoginUser String studentId,
+		@Valid @RequestBody DepartmentInfoUpdateRequest request) {
+		departmentService.updateDepartmentInfo(studentId, request);
 		return ResponseEntity.ok().build();
 	}
 
-	@PatchMapping
+	@PutMapping("/rentaltime")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Void> updateDepartment(@LoginUser String studentId,
-		@Valid @RequestBody DepartmentUpdateRequest request) {
+	public ResponseEntity<Void> updateDepartmentRentalTime(@LoginUser String studentId,
+		@Valid @RequestBody DepartmentRentalTimeUpdateRequest request) {
+		departmentService.updateDepartmentRentalTime(studentId, request);
 		return ResponseEntity.ok().build();
 	}
 }
