177ddc40ab66dfcd0de22f425928c940aa6b47a4
Jaewon Kim <eshcirc1e@gmail.com>
2024-08-08 19:16:37 +0900

    feat: Rental 도메인 Controller, Service, Repository 임시 구현 및 Rental 생성 기능 구현

--- src/main/java/mango/rentalsystem/domain/rental/api/RentalController.java
+++ src/main/java/mango/rentalsystem/domain/rental/api/RentalController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mango.rentalsystem.domain.auth.domain.LoginUser;
 import mango.rentalsystem.domain.rental.application.RentalService;
 import mango.rentalsystem.domain.rental.domain.Rental;
+import mango.rentalsystem.domain.rental.dto.request.AdminRentalStatusUpdateRequest;
 import mango.rentalsystem.domain.rental.dto.request.RentalRequest;
+import mango.rentalsystem.domain.rental.dto.request.MemberRentalStatusUpdateRequest;
+import mango.rentalsystem.domain.rental.dto.response.RentalCreateResponse;
 
 @RestController
 @RequestMapping("/rental")
@@ -24,27 +29,42 @@ public class RentalController {
 
 	private final RentalService rentalService;
 
-	@GetMapping
-	@PreAuthorize("hasRole('MEMBER')")
-	public ResponseEntity<List<Rental>> getRentals() {
-		return null;
-	}
-
 	@PostMapping
 	@PreAuthorize("hasRole('MEMBER')")
-	public ResponseEntity<Void> postRental(@Valid @RequestBody RentalRequest rentalRequest) {
-		return null;
+	public ResponseEntity<RentalCreateResponse> createRental(@LoginUser String studentId,
+		@RequestBody @Valid RentalRequest rentalRequest) {
+		return ResponseEntity.ok(rentalService.createRental(studentId, rentalRequest));
 	}
 
-	@PatchMapping
-	@PreAuthorize("hasRole('MEMBER')")
-	public ResponseEntity<Void> patchRentalStatus(@Valid @RequestBody RentalRequest rentalRequest) {
-		return null;
-	}
-
-	@PatchMapping
+	@GetMapping
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Void> patchRentalStatus(@Valid @RequestBody RentalRequest rentalRequest) {
+	public ResponseEntity<List<Rental>> getAllRentals() {
+		return null;
+	}
+
+	@GetMapping("/myrental")
+	@PreAuthorize("hasRole('MEMBER')")
+	public ResponseEntity<List<Rental>> getMyRentals(@LoginUser String studentId) {
+		return null;
+	}
+
+	@GetMapping("/{rentalId}")
+	@PreAuthorize("hasRole('MEMBER')")
+	public ResponseEntity<Rental> getRental(@LoginUser String studentId, @PathVariable Long rentalId) {
+		return null;
+	}
+
+	@PatchMapping("/{rentalId}")
+	@PreAuthorize("hasRole('MEMBER')")
+	public ResponseEntity<Void> updateRentalStatusByMember(@PathVariable Long rentalId,
+		@RequestBody @Valid MemberRentalStatusUpdateRequest request) {
+		return null;
+	}
+
+	@PatchMapping("/{rentalId}/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Void> updateRentalStatusByAdmin(@PathVariable Long rentalId,
+		@RequestBody @Valid AdminRentalStatusUpdateRequest request) {
 		return null;
 	}
 }
--- src/main/java/mango/rentalsystem/domain/rental/application/RentalService.java
+++ src/main/java/mango/rentalsystem/domain/rental/application/RentalService.java
@@ -1,12 +1,106 @@
 package mango.rentalsystem.domain.rental.application;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import mango.rentalsystem.domain.department.domain.DailyRentalTime;
+import mango.rentalsystem.domain.item.dao.ItemRepository;
+import mango.rentalsystem.domain.item.domain.Item;
+import mango.rentalsystem.domain.member.dao.MemberRepository;
+import mango.rentalsystem.domain.member.domain.Member;
+import mango.rentalsystem.domain.rental.dao.RentalRepository;
+import mango.rentalsystem.domain.rental.domain.Rental;
+import mango.rentalsystem.domain.rental.domain.RentalStatus;
+import mango.rentalsystem.domain.rental.dto.request.RentalRequest;
+import mango.rentalsystem.domain.rental.dto.request.RentalStatusUpdateRequest;
+import mango.rentalsystem.domain.rental.dto.response.RentalCreateResponse;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class RentalService {
+
+	private final MemberRepository memberRepository;
+	private final RentalRepository rentalRepository;
+	private final ItemRepository itemRepository;
+
+	public RentalCreateResponse createRental(String studentId, RentalRequest request) {
+		Member member = memberRepository.findByStudentId(studentId)
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다."));
+
+		DailyRentalTime todayRentalTime = member.getDepartment()
+			.getWeeklyRentalTime()
+			.get(LocalDate.now().getDayOfWeek());
+
+		if (LocalTime.now().isBefore(todayRentalTime.getRentalStartTime()) &&
+			LocalTime.now().isAfter(todayRentalTime.getRentalEndTime())) {
+			throw new RuntimeException("대여 가능 시간이 아닙니다.");
+		}
+
+		Item item = itemRepository.findById(request.itemId())
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 물품입니다."));
+
+		Rental rental = Rental.createInitialRental(member, item);
+		Rental savedRental = rentalRepository.save(rental);
+
+		return RentalCreateResponse.from(savedRental);
+	}
+
+	public void updateRentalStatus(Long rentalId, RentalStatusUpdateRequest request) {
+		Rental rental = rentalRepository.findById(rentalId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 대여 Id 입니다."));
+
+		RentalStatus currentStatus = rental.getRentalStatus();
+		RentalStatus requestStatus = request.rentalStatus();
+
+		switch (currentStatus) {
+			case APPROVAL_REQUESTED -> {
+				switch (requestStatus) {
+					case APPROVED -> {
+					}
+					case REJECTED -> {
+					}
+					case CANCELED -> {
+					}
+					default -> {
+					}
+				}
+			}
+			case APPROVED -> {
+				switch (requestStatus) {
+					case BORROW -> {
+					}
+					default -> {
+					}
+				}
+			}
+			case BORROW, OVERDUE -> {
+				switch (requestStatus) {
+					case RETURN_REQUESTED -> {
+					}
+					default -> {
+					}
+				}
+			}
+			case RETURN_REQUESTED -> {
+				switch (requestStatus) {
+					case RETURN -> {
+						rental.updateRentalStatus;
+					}
+					default -> {
+					}
+				}
+			}
+			default -> {
+			}
+		}
+	}
+
+	public void updateRentalStatusToOverdue() {
+
+	}
 }
