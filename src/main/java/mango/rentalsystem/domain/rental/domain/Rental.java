05663d9f98e9ea3e90ac457373d23fa47ae13ceb
Jaewon Kim <eshcirc1e@gmail.com>
2024-08-08 19:03:52 +0900

    feat: Rental 엔티티에 반납 기한 필드 추가, builder 패턴 적용 및 정적 팩토리 메서드 추가

--- src/main/java/mango/rentalsystem/domain/rental/domain/Rental.java
+++ src/main/java/mango/rentalsystem/domain/rental/domain/Rental.java
@@ -1,11 +1,15 @@
 package mango.rentalsystem.domain.rental.domain;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import mango.rentalsystem.domain.department.domain.DailyRentalTime;
+import mango.rentalsystem.domain.department.domain.Department;
 import mango.rentalsystem.domain.item.domain.Item;
 import mango.rentalsystem.domain.member.domain.Member;
 
@@ -34,5 +38,42 @@ public class Rental {
 
 	private LocalDateTime returnDateTime;
 
+	private LocalDateTime deadlineDateTime;
+
 	private int rentalReview;
+
+	@Builder(access = AccessLevel.PRIVATE)
+	private Rental(Member member, Item item, RentalStatus rentalStatus, LocalDateTime borrowDateTime,
+		LocalDateTime returnDateTime, LocalDateTime deadlineDateTime, int rentalReview) {
+		this.member = member;
+		this.item = item;
+		this.rentalStatus = rentalStatus;
+		this.borrowDateTime = borrowDateTime;
+		this.returnDateTime = returnDateTime;
+		this.deadlineDateTime = deadlineDateTime;
+		this.rentalReview = rentalReview;
+	}
+
+	public static Rental createInitialRental(Member member, Item item) {
+		return Rental.builder()
+			.member(member)
+			.item(item)
+			.rentalStatus(RentalStatus.APPROVAL_REQUESTED)
+			.borrowDateTime(LocalDateTime.now())
+			.deadlineDateTime(calculateDeadlineDateTime(member))
+			.build();
+	}
+
+	private static LocalDateTime calculateDeadlineDateTime(Member member) {
+		DailyRentalTime todayRentalTime = member.getDepartment()
+			.getWeeklyRentalTime()
+			.get(LocalDate.now().getDayOfWeek()); // DailyRentalTime 클래스에 메서드 만들기.
+		return LocalDateTime.of(LocalDate.now().plusDays(todayRentalTime.getRentalDeadLineDate()),
+			todayRentalTime.getRentalDeadLineTime());
+	}
+
+	public void updateRentalStatusToReturn() {
+		this.rentalStatus = RentalStatus.RETURN;
+		this.returnDateTime = LocalDateTime.now();
+	}
 }
