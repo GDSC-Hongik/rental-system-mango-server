248b4473505dbd875336d2f9bc1dd8fe0f95acd7
Jaewon Kim <eshcirc1e@gmail.com>
2024-08-11 21:54:50 +0900

    feat: 대여 생성 요청 및 응답 dto, 대여 조회 응답 dto, 대여 평점 요청 dto 구현

--- src/main/java/mango/rentalsystem/domain/rental/dto/request/RentalRequest.java
+++
@@ -1,6 +0,0 @@
-package mango.rentalsystem.domain.rental.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record RentalRequest(@NotNull(message = "아이템 id를 입력해주세요") Long itemId) {
-}
--- src/main/java/mango/rentalsystem/domain/rental/dto/response/RentalFindResponse.java
+++ src/main/java/mango/rentalsystem/domain/rental/dto/response/RentalFindResponse.java
@@ -5,24 +5,35 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import mango.rentalsystem.domain.item.domain.Item;
+import mango.rentalsystem.domain.member.domain.Member;
 import mango.rentalsystem.domain.rental.domain.Rental;
 import mango.rentalsystem.domain.rental.domain.RentalStatus;
 
-public record RentalCreateResponse(
+public record RentalFindResponse(
 	Long rentalId,
+	String studentId,
 	Long itemId,
 	RentalStatus rentalStatus,
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	LocalDateTime requestDateTime,
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-	LocalDateTime deadlineDateTime) {
+	LocalDateTime borrowDateTime,
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	LocalDateTime returnDateTime,
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	LocalDateTime deadlineDateTime,
+	Integer rentalReview) {
 
-	public static RentalCreateResponse from(Rental rental) {
-		return new RentalCreateResponse(
+	public static RentalFindResponse from(Rental rental) {
+		return new RentalFindResponse(
 			rental.getId(),
+			rental.getMember().getStudentId(),
 			rental.getItem().getId(),
 			rental.getRentalStatus(),
 			rental.getRequestDateTime(),
-			rental.getDeadlineDateTime());
+			rental.getBorrowDateTime(),
+			rental.getReturnDateTime(),
+			rental.getDeadlineDateTime(),
+			rental.getRentalReview());
 	}
 }
