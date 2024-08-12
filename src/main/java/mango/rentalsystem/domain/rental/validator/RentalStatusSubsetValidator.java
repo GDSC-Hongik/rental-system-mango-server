3e502ab8800f2c4b7af06e8c55be9408b43b846e
Jaewon Kim <eshcirc1e@gmail.com>
2024-08-13 02:31:17 +0900

    feat: RentalStatusSubsetValidator 검증 로직 간결화 및 예외 메시지 설정

--- src/main/java/mango/rentalsystem/domain/rental/validator/RentalStatusSubsetValidator.java
+++ src/main/java/mango/rentalsystem/domain/rental/validator/RentalStatusSubsetValidator.java
@@ -1,5 +1,7 @@
 package mango.rentalsystem.domain.rental.validator;
 
+import static mango.rentalsystem.domain.member.domain.MemberRole.*;
+
 import java.util.Arrays;
 
 import org.springframework.security.core.Authentication;
@@ -23,26 +25,30 @@ public class RentalStatusSubsetValidator implements ConstraintValidator<RentalSt
 	}
 
 	@Override
-	public boolean isValid(RentalStatus rentalStatus, ConstraintValidatorContext constraintValidatorContext) {
+	public boolean isValid(RentalStatus rentalStatus, ConstraintValidatorContext context) {
 		MemberRole memberRole = currentMemberRole();
-		if (memberRole == MemberRole.ROLE_MEMBER) {
-			if (memberSubset == null) {
-				return true;
-			}
-			return Arrays.asList(memberSubset).contains(rentalStatus);
-		} else if (memberRole == MemberRole.ROLE_ADMIN) {
-			if (adminSubset == null) {
-				return true;
-			}
-			return Arrays.asList(adminSubset).contains(rentalStatus);
-		} else {
-			throw new IllegalStateException("MemberRole 값에 오류가 있습니다. MemberRole: " + memberRole);
+		if (
+			(memberRole == ROLE_MEMBER)
+				&& !(memberSubset == null || Arrays.asList(memberSubset).contains(rentalStatus))
+			|| (memberRole == ROLE_ADMIN)
+				&& !(adminSubset == null || Arrays.asList(adminSubset).contains(rentalStatus))
+		) {
+			setRoleErrorMessage(context, memberRole);
+			return false;
 		}
+		return true;
 	}
 
 	private MemberRole currentMemberRole() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
-		return MemberRole.valueOf(authDetails.getMemberRole());
+		AuthDetails authDetails = (AuthDetails)authentication.getPrincipal();
+		return valueOf(authDetails.getMemberRole());
+	}
+
+	private void setRoleErrorMessage(ConstraintValidatorContext context, MemberRole memberRole) {
+		RentalStatus[] subset = memberRole == ROLE_MEMBER ? memberSubset : adminSubset;
+		context.disableDefaultConstraintViolation();
+		context.buildConstraintViolationWithTemplate(memberRole.name() + "은 " + Arrays.toString(
+			subset) + " 중 하나의 상태로만 요청이 가능합니다.");
 	}
 }
