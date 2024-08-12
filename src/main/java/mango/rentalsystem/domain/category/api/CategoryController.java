82f3dac13d7cc23e23f93c599744fc15ee41e8b6
yeonee911 <128215774+yeonee911@users.noreply.github.com>
2024-08-12 15:03:12 +0900

    style: uri 오타 수정

--- src/main/java/mango/rentalsystem/domain/category/api/CategoryController.java
+++ src/main/java/mango/rentalsystem/domain/category/api/CategoryController.java
@@ -55,7 +55,7 @@ public class CategoryController {
 
 	// 특정 카테고리 정보 변경
 	@PreAuthorize("hasRole('ADMIN')")
-	@PatchMapping("{/categoryId}")
+	@PatchMapping("/{categoryId}")
 	public ResponseEntity<Void> modifyCategory(@Valid @RequestBody CategoryModifyRequest request, @PathVariable Long categoryId) {
 		categoryService.modifyCategory(request, categoryId);
 		return ResponseEntity.ok().build();
