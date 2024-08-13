6034b1f2ad517e1208737debdbaf7cd676dd461f
ehddn6570 <ehddn6570@gmail.com>
2024-08-14 01:42:08 +0900

    fix:memberDto 수정

--- src/main/java/mango/rentalsystem/domain/member/dto/UpdateMemberRequest.java
+++ src/main/java/mango/rentalsystem/domain/member/dto/UpdateMemberRequest.java
@@ -1,2 +1,15 @@
-package mango.rentalsystem.domain.member.dto;public class UpdateMemberRequest {
+package mango.rentalsystem.domain.member.dto;
+
+import lombok.Data;
+
+
+// 멤버 수정
+@Data
+public class UpdateMemberRequest {
+
+	private String password;
+	private String studentId;
+	private String name;
+	private String phone;
+	private Boolean absenceStatus;
 }
