088da0fcc37acce89a6a999e032e9494819f2654
Jaewon Kim <eshcirc1e@gmail.com>
2024-08-14 17:34:57 +0900

    feat: ItemStatus의 @Enumerated 옵션 String 으로 적용

--- src/main/java/mango/rentalsystem/domain/item/domain/Item.java
+++ src/main/java/mango/rentalsystem/domain/item/domain/Item.java
@@ -1,9 +1,9 @@
 package mango.rentalsystem.domain.item.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -31,6 +31,7 @@ public class Item {
 	@JoinColumn(name = "category_id")
 	private Category category;
 
+	@Enumerated(EnumType.STRING)
 	private ItemStatus itemStatus;
 
 	private Double itemReview;
