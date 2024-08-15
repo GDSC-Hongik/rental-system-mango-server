64215227245b43cd5cb25e93a0bbc1b0b8132ab0
ehddn6570 <ehddn6570@gmail.com>
2024-08-16 03:48:00 +0900

    fix: memeber 엔티티  수정

--- src/main/java/mango/rentalsystem/domain/member/domain/Member.java
+++ src/main/java/mango/rentalsystem/domain/member/domain/Member.java
@@ -2,27 +2,23 @@ package mango.rentalsystem.domain.member.domain;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import mango.rentalsystem.domain.department.domain.Department;
-import mango.rentalsystem.domain.rental.domain.Rental;
+import mango.rentalsystem.global.exception.CustomException;
+import mango.rentalsystem.global.exception.ErrorCode;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "member_id")
 	private Long id;
 
@@ -32,11 +28,8 @@ public class Member {
 
 	private String name;
 
-
 	@Enumerated(EnumType.STRING)
-	@Builder.Default
-	private MemberRole role = MemberRole.ROLE_MEMBER;
-
+	private MemberRole role;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "department_id")
@@ -46,13 +39,67 @@ public class Member {
 
 	private String pictureUrl;
 
-	private boolean absenceStatus;
+	private Boolean absenceStatus;
 
-	@OneToMany(mappedBy = "member")
-	@Builder.Default
-	private List<Rental> rentalList = new ArrayList<>();
+	private LocalDateTime rentalBannedDate;
 
-	private LocalDate rentalBannedDate;
+	@Builder(access = AccessLevel.PRIVATE)
+	private Member(String studentId, String password, String name, MemberRole role, Department department, String phone,
+		String pictureUrl, Boolean absenceStatus, LocalDateTime rentalBannedDate) {
+		this.studentId = studentId;
+		this.password = password;
+		this.name = name;
+		this.role = role;
+		this.department = department;
+		this.phone = phone;
+		this.pictureUrl = pictureUrl;
+		this.absenceStatus = absenceStatus;
+		this.rentalBannedDate = rentalBannedDate;
+	}
 
+	public static Member createMember(String studentId, String password, String name, Department department,
+		String phone) {
+		return Member.builder()
+			.studentId(studentId)
+			.password(password)
+			.name(name)
+			.role(MemberRole.ROLE_MEMBER)
+			.department(department)
+			.phone(phone)
+			.absenceStatus(false)
+			.build();
+	}
 
+	public static Member createAdmin(String studentId, String password, String name, Department department,
+		String phone) {
+		return Member.builder()
+			.studentId(studentId)
+			.password(password)
+			.name(name)
+			.role(MemberRole.ROLE_ADMIN)
+			.department(department)
+			.phone(phone)
+			.absenceStatus(false)
+			.build();
+	}
+
+	public void updateMemberInfo(String name, String phone, Boolean absenceStatus) {
+		this.name = name;
+		this.phone = phone;
+		this.absenceStatus = absenceStatus;
+	}
+
+	public void updateMemberPassword(String password) {
+		this.password = password;
+	}
+
+	public void validateRentalBannedDate() {
+		if (rentalBannedDate != null && LocalDateTime.now().isBefore(this.rentalBannedDate)) {
+			throw new CustomException(ErrorCode.RENTAL_BANNED);
+		}
+	}
+
+	public void updateRentalBannedDate() {
+		this.rentalBannedDate = LocalDateTime.now().plusDays(3);
+	}
 }
