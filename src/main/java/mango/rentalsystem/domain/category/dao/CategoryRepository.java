1fecd96e7079c1fc162198519bd8f3c327922eb8
Jaewon Kim <eshcirc1e@gmail.com>
2024-08-15 20:05:29 +0900

    feat: 카테고리 도메인에 현재 로그인 유저 정보 적용

--- src/main/java/mango/rentalsystem/domain/category/dao/CategoryRepository.java
+++ src/main/java/mango/rentalsystem/domain/category/dao/CategoryRepository.java
@@ -1,5 +1,6 @@
 package mango.rentalsystem.domain.category.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 	Optional<Category> findByNameAndDepartment(String categoryName, Department department);
 
-	Optional<Category> findById(Long categoryId);
-
-	void deleteById(Long categoryId);
+	List<Category> findAllByDepartment(Department department);
 }
